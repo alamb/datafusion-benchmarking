5b1f42cced6e3c9f143662af9f58950635d17dbe
alamb <andrew@nerdnetworks.org>
2025-04-04 15:35:59 +0000

    update

---
+++ poll.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Script looks for scripts in the jobs directory and runs then
+# on completion renames to `job.done` to avoid rerunning
+#
+# AKA ghetto job scheduler
+
+set -x -e
+
+mkdir -p jobs
+while true ; do
+    echo "Checking for jobs"
+    for job in `ls jobs/*.sh` ; do
+        echo "Running job $job"
+        bash "$job"
+        echo "Renaming $job to $job.done"
+        mv "$job" "$job.done"
+    done
+    sleep 1
+done;
