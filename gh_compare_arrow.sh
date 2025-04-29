0c1730badd893c8beb33d0a9857fac3487206e7c
alamb <andrew@nerdnetworks.org>
2025-04-29 12:29:29 +0000

    Add arrow bench compare

--- gh_compare_arrow.sh
+++ gh_compare_arrow.sh
@@ -1,16 +1,18 @@
-set -e -x
+set -x -e
 ##
-# This script runs a datafusion --bench microbenchmark
+# This script runs an arrow-rs --bench microbenchmark and posts the results
+# to github tickets
+#
 # Usage
 #
-# gh_compare_branch.sh <$PR_URL>
-# BENCH_NAME="sql_planner" gh_compare_branch_bench.sh <$PR_URL>
+# gh_compare_arrow.sh  <$PR_URL>
+# BENCH_NAME="<name>" BENCH_FILTER="args" gh_compare_arrow.sh <$PR_URL>
 #
 # Example
-# https://github.com/apache/datafusion/pull/15466
+# BENCH_NAME=concatente_kernel gh_compare_arrow.sh https://github.com/apache/datafusion/pull/15466
 #
 # Uses directories like this
-# ~/arrow-datafusion2: branch + main
+# ~/arrow-rs: branch + main
 #
 # And then reports the results to a pull request using the gh command line
 #
@@ -19,28 +21,21 @@ set -e -x
 # https://github.com/cli/cli/releases/download/v2.69.0/gh_2.69.0_linux_amd64.deb
 ##
 
-# setup python environment
-source ~/venv/bin/activate
-
-
 PR=$1
 if [ -z "$PR" ] ; then
     echo "gh_compare_branch_bench.sh <$PR_URL>"
 fi
 
-## Benchmarks to run
-BENCH_NAME=${BENCH_NAME:-"sql_planner"}
+# Benchmarks to run
+BENCH_NAME=${BENCH_NAME:-"concatenate_kernel"}
 BENCH_FILTER=${BENCH_FILTER:-""}
-BENCH_COMMAND="cargo bench --bench $BENCH_NAME"
-
-## Command used to pre-warm (aka precompile) the directories
-CARGO_COMMAND="cargo run --release"
+BENCH_COMMAND="cargo bench --all-features --bench $BENCH_NAME "
 
 ######
-# Fetch and checkout the remote branch in arrow-datafusion2
+# Fetch and checkout the remote branch in arrow-rs
 ######
+pushd ~/arrow-rs
 
-pushd ~/arrow-datafusion2
 git reset --hard
 git clean -f -d
 git fetch -p apache
@@ -49,11 +44,12 @@ MERGE_BASE=`git merge-base HEAD apache/main`
 BRANCH_BASE=`git rev-parse HEAD`
 BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
 BENCH_BRANCH_NAME=${BRANCH_NAME//\//_} # mind blowing syntax to replace / with _
+cargo update
 
 # create comment saying the benchmarks are running
 rm -f /tmp/comment.txt
 cat >/tmp/comment.txt <<EOL
-🤖 \`$0\` [Benchmark Script](https://github.com/alamb/datafusion-benchmarking/blob/main/gh_compare_branch_bench.sh) Running
+🤖 \`$0\` [Benchmark Script](https://github.com/alamb/datafusion-benchmarking/blob/main/gh_compare_arrow.sh) Running
 `uname -a`
 Comparing $BRANCH_NAME ($BRANCH_BASE) to $MERGE_BASE [diff](https://github.com/apache/datafusion/compare/$MERGE_BASE..$BRANCH_BASE)
 BENCH_NAME=$BENCH_NAME
@@ -65,18 +61,18 @@ EOL
 # Post the comment to the ticket
 gh pr comment -F /tmp/comment.txt $PR
 
-# remove old runs
+# remove old test runs, if any
 rm -rf target/criterion/
 
 # Run on test branch
-$BENCH_COMMAND -- --save-baseline ${BENCH_BRANCH_NAME} ${BENCH_FILTER}
+$BENCH_COMMAND -- --save-baseline ${BRANCH_DISPLAY_NAME} ${BENCH_FILTER}
+
 
 # Run on main (merge base)
 git reset --hard
 git clean -f -d
 git checkout $MERGE_BASE
 $BENCH_COMMAND -- --save-baseline main  ${BENCH_FILTER}
-
 
 ## Compare
 rm -f /tmp/report.txt
