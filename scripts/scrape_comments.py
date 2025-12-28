65f9979cb511e01232c94d9a367faa372973fd6a
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2025-12-28 20:32:29 +0200

    Add 'row_format' to arrow benchmarks

--- scripts/scrape_comments.py
+++ scripts/scrape_comments.py
@@ -95,6 +95,7 @@ ALLOWED_CRITERION_BENCHMARKS_ARROW = {
     "variant_validation",
     "filter_kernels",
     "concatenate_kernel",
+    "row_format",
 }
 
 DEFAULT_REPOS = "apache/datafusion:apache/arrow-rs"
